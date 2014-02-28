d48c875ead2d15487d76fb277007db3cfd5a3f3d
Jerry <jerry.j@multunus.com>
2014-02-28 11:33:02 +0530

    removed shadow

--- lib/horizontal_scroll_controller/basic_table_view_cell.rb
+++ lib/horizontal_scroll_controller/basic_table_view_cell.rb
@@ -5,10 +5,9 @@ class BasicTableViewCell < UITableViewCell
 
   def initWithFrame(frame)
     if super
-      self.thumbnail = ImageViewWithOverlay.alloc.initWithFrame(CGRectMake(15, 23, 115, 145))
+      self.thumbnail = UIView.alloc.initWithFrame(CGRectMake(15, 23, 115, 145))
       self.thumbnail.userInteractionEnabled = true
       self.thumbnail.opaque = true
-      apply_shadow_to_thumbnail
       self.contentView.addSubview(thumbnail)
       self.backgroundColor = CELL_BACKGROUND_COLOR
       self.selectedBackgroundView = UIView.alloc.initWithFrame(self.thumbnail.frame)
@@ -23,12 +22,6 @@ class BasicTableViewCell < UITableViewCell
 
   end
 
-  def apply_shadow_to_thumbnail
-    path = UIBezierPath.bezierPathWithRect(self.thumbnail.bounds)
-    shadow_radius = 3
-    make_shadow_with_path(self.thumbnail, "black", 0.4, shadow_radius, CGSizeMake(2,2), path, false)
-  end
-
   def set_thumbnail_image image
     self.thumbnail.image = UIImage.imageNamed(image)
   end
@@ -37,14 +30,4 @@ class BasicTableViewCell < UITableViewCell
     self.thumbnail.setImageWithURL(NSURL.URLWithString(image_url), placeholderImage: UIImage.imageNamed(placeholder))
   end
 
-  def make_shadow_with_path(view, color, opacity, radius, offset, bezier_path, rasterize)
-    view.layer.shouldRasterize = rasterize
-    view.layer.shadowColor = color.to_color.CGColor
-    view.layer.masksToBounds = false
-    view.layer.shadowOffset = offset
-    view.layer.shadowOpacity = opacity
-    view.layer.shadowRadius = radius
-    view.layer.shadowPath = bezier_path.CGPath
-    view.layer.rasterizationScale = UIScreen.mainScreen.scale
-  end
 end
